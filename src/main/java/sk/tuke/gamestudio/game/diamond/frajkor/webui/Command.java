41ebf06e864fc0e623e39326cd7c1995614fc403
Martin Frajkor <martin.frajkor@student.tuke.sk>
2020-05-08 19:30:35 +0200

    reworked the whole project to new version because i cant get variable reference to js from th

--- src/main/java/sk/tuke/gamestudio/game/diamond/frajkor/webui/Command.java
+++
@@ -1,5 +0,0 @@
-package sk.tuke.gamestudio.game.diamond.frajkor.webui;
-
-public enum Command {
-    NEW, PLACE, MOVE, DRAW
-}
--- src/main/java/sk/tuke/gamestudio/server/controller/DiamondFrajkorController.java
+++ src/main/java/sk/tuke/gamestudio/server/controller/DiamondFrajkorController.java
@@ -1,16 +1,17 @@
 package sk.tuke.gamestudio.server.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.context.annotation.Scope;
-import sk.tuke.gamestudio.entity.Comment;
-import sk.tuke.gamestudio.entity.Rating;
-import sk.tuke.gamestudio.entity.Score;
-import sk.tuke.gamestudio.game.diamond.frajkor.webui.Command;
-import sk.tuke.gamestudio.game.diamond.frajkor.webui.WebUI;
+import sk.tuke.gamestudio.game.diamond.frajkor.core.Field;
+import sk.tuke.gamestudio.game.diamond.frajkor.core.GamePhase;
+import sk.tuke.gamestudio.game.diamond.frajkor.core.GameState;
+import sk.tuke.gamestudio.game.diamond.frajkor.core.Piece;
+import sk.tuke.gamestudio.game.diamond.frajkor.core.exceptions.gamephase.WrongGamePhaseException;
+import sk.tuke.gamestudio.game.diamond.frajkor.core.exceptions.pieces.PiecesException;
 import sk.tuke.gamestudio.service.comment.CommentService;
 import sk.tuke.gamestudio.service.rating.RatingService;
 import sk.tuke.gamestudio.service.score.ScoreService;
@@ -19,52 +20,106 @@ import java.util.List;
 
 @Controller
 @Scope(WebApplicationContext.SCOPE_SESSION)
+@RequestMapping("/diamond-frajkor")
 public class DiamondFrajkorController {
 
-    private final WebUI webUI;
+    @Autowired
+    private ScoreService scoreService;
 
-    private final ScoreService scoreService;
-    private final CommentService commentService;
-    private final RatingService ratingService;
+    @Autowired
+    private CommentService commentService;
 
-    public DiamondFrajkorController(ScoreService scoreService, CommentService commentService, RatingService ratingService) {
-        this.scoreService = scoreService;
-        this.commentService = commentService;
-        this.ratingService = ratingService;
-        this.webUI = new WebUI(scoreService,ratingService,commentService);
-    }
+    @Autowired
+    private RatingService ratingService;
 
-    @RequestMapping("/diamond-frajkor")
-    public String diamond(@RequestParam(value = "command", required = false) String command,
-                          @RequestParam(value = "row", required = false) String rowFrom,
-                          @RequestParam(value = "column", required = false) String columnFrom,
-//                          @RequestParam(value = "rowTo", required = false) String rowTo,
-//                          @RequestParam(value = "columnTo", required = false) String columnTo,
-                          Model model) {
+    private static final String GAME_NAME = "diamond-frajkor";
+    private Field field;
 
-        Command cmd;
-
-        if (command == null) {
-            cmd = ((rowFrom != null) && (columnFrom != null)) ? Command.PLACE : Command.NEW;
-        }else if(command.equals("new")){
-            cmd = Command.NEW;
-        }else if(webUI.isPlacementPhase()){
-            cmd = Command.PLACE;
-        }else{
-            cmd = Command.MOVE;
+    @RequestMapping
+    public String diamond(String row, String column, Model model) {
+        if(this.field == null) {
+            createNewGame();
         }
 
-        webUI.processCommand(cmd, rowFrom, columnFrom);
-        model.addAttribute("webUI",webUI);
-        List<Score> bestScores = scoreService.getBestScores("diamond-frajkor");
-        List<Comment> comments = commentService.getComments("diamond-frajkor");
-        int ratings = ratingService.getAverageRating("diamond-frajkor");
+        try{
+                if(field.getGamePhase() == GamePhase.PLACEMENT){
+                    field.placePiece(Integer.parseInt(row),Integer.parseInt(column));
+                }
+                else{
+                    field.movePiece(Integer.parseInt(row),Integer.parseInt(column),0,0);
+                }
+            }catch (NumberFormatException | PiecesException | WrongGamePhaseException | NullPointerException e){
+                e.printStackTrace();
+            }
 
-        model.addAttribute("scores", bestScores);
-        model.addAttribute("comments",comments);
-        model.addAttribute("averageRating",ratings);
-
+        prepareModel(model);
         return "diamond-frajkor";
+    }
+
+    @RequestMapping("/new")
+    public String newGame(Model model) {
+        createNewGame();
+        prepareModel(model);
+        return "diamond-frajkor";
+    }
+
+    public GameState getGameState() {
+        return field.getGameState();
+    }
+
+    public GamePhase getGamePhase(){
+        return field.getGamePhase();
+    }
+
+    public String getHtmlField() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<table class='field'>\n");
+        for (int row = 0; row < field.getRowCount(); row++) {
+            sb.append("<tr class='field-row'>\n");
+            for (int column = 0; column < field.getColumnCount(row); column++) {
+                Piece piece = field.getPiece(row, column);
+                sb.append("<td>\n");
+                if(getGamePhase() == GamePhase.PLACEMENT) {
+                    sb.append("<a href='" +
+                            String.format("/diamond-frajkor?row=%s&column=%s", row, column)
+                            + "' class='piece-link'>");
+                    sb.append("<div class='circle piece-" + getClassName(piece) + "'></div>");
+                    sb.append("</a>\n");
+                }
+                sb.append("</td>\n");
+            }
+            sb.append("</tr>\n");
+        }
+        sb.append("</table>\n");
+
+        return sb.toString();
+    }
+
+
+    private void createNewGame() {
+        this.field = new Field();
+    }
+
+    private String getClassName(Piece piece) {
+        switch (piece.getPieceType()) {
+            case EMPTY:
+                return "empty";
+            case BLACK:
+                return "black";
+            case WHITE:
+                return "white";
+            case NEUTRAL:
+                return "neutral";
+        }
+        throw new IllegalArgumentException("State not supported" + piece.getPieceType());
+    }
+
+    private void prepareModel(Model model) {
+        model.addAttribute("scores", scoreService.getBestScores(GAME_NAME));
+        model.addAttribute("averageRating", ratingService.getAverageRating(GAME_NAME));
+        model.addAttribute("playerRating", ratingService.getRating(GAME_NAME, System.getProperty("user.name")));
+        model.addAttribute("comments", commentService.getComments(GAME_NAME));
+        model.addAttribute("gameField",field.getField());
     }
 
 
