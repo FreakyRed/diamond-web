2b1773c4ba2fe5f6a4173d8e24afd683599dc769
Martin Frajkor <martin.frajkor@student.tuke.sk>
2020-05-13 23:16:13 +0200

    finishing touches on the ui, might add picture as thumbnail

--- src/main/java/sk/tuke/gamestudio/service/user/UserServiceJPA.java
+++ src/main/java/sk/tuke/gamestudio/service/user/UserServiceJPA.java
@@ -4,12 +4,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 import sk.tuke.gamestudio.entity.User;
 
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.persistence.PersistenceContext;
+import javax.persistence.*;
+import java.sql.SQLException;
 
 @Transactional
-public class UserServiceJPA implements UserService{
+public class UserServiceJPA implements UserService {
 
     @PersistenceContext
     private EntityManager entityManager;
@@ -18,37 +17,41 @@ public class UserServiceJPA implements UserService{
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
     @Override
-    public void register(String username, String password){
+    public void register(String username, String password) {
         User user = new User(username, encoder.encode(password));
-        entityManager.persist(user);
+        if (entityManager.contains(user)) {
+            throw new UserException("User already exists");
+        } else {
+            entityManager.persist(user);
+        }
     }
 
     @Override
-    public User login(String username){
-        try{
+    public User login(String username) {
+        try {
             User loggedUser = (User) entityManager.createNamedQuery("User.getLogin")
-                    .setParameter("username",username)
+                    .setParameter("username", username)
                     .getSingleResult();
             user = loggedUser;
             return loggedUser;
-        }catch (NoResultException e){
+        } catch (NoResultException e) {
             throw new UserException("User does not exist in the database..");
         }
     }
 
     @Override
     public boolean isPasswordVerified(String username, String password) {
-        try{
+        try {
             String hash = (String) entityManager.createNamedQuery("User.getHash")
-                    .setParameter("username",username)
+                    .setParameter("username", username)
                     .getSingleResult();
-            return encoder.matches(password,hash);
-        }catch (NoResultException e){
-            throw new UserException("Password does not match.");
+            return encoder.matches(password, hash);
+        } catch (NoResultException e) {
+            throw new UserException("Password does not match your account");
         }
     }
 
-    public User getUser(){
+    public User getUser() {
         return user;
     }
 
